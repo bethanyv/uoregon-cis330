49989c69432dfba9aa60598290d8734dda3e3d16
Bethany <bethanyv21@gmail.com>
2019-01-12 22:13:53 -0800

    finished sticks.c!

--- assignment1/sticks.c
+++ assignment1/sticks.c
@@ -7,26 +7,20 @@ int getUserChoice() {
   int num;
   printf("Player 1: How many sticks do you take (1-3)? ");
   scanf("%d", &num);
-  while(num > 3 || num < 1) {
+
+  // if input is invalid, ask again
+  while(num > 3 || num < 1) { 
     printf("Invalid input, please input a number (1-3): ");
     scanf("%d", &num);
   }
   return num;
-  /* TODO: Prompt the user "Player 1: How many sticks do you take (1-3)?" and return
-     an integer. Check that the value is valid (1, 2, or 3) and print an error if 
-     is not, then ask again until a valid number is entered. You can exit the program with
-     exit(1);
-  */
 }
 
 int getComputerChoice(int current_sticks) {
-
     /* get a pseudo-random integer between 1 and 3 (inclusive) */
     int rand_choice = rand() % 3 + 1;
 
     if (rand_choice > current_sticks) return current_sticks;
-
-    /* Optionally replace with additional logic for the computer's strategy */
 
     return rand_choice;
 }
@@ -36,47 +30,45 @@ int main(int argc, char** argv)
 {
   int user, computer, number_sticks;
 
-
-  srand (time(NULL)); /* for reproducible results, you can call srand(1); */
+  srand (time(NULL));
 
   printf("Welcome to the game of sticks!\n");
   printf("How many sticks are there on the table initially (10-100)? ");
   scanf("%d", &number_sticks);
 
-  /* TODO: check that num_sticks is between 10 and 100 (inclusive) and print 
-    an error and exit, if it is not. */
+  /* if sticks on table initially don't fit in with range 10-100, 
+  print error and quit*/
   if (number_sticks < 10 || number_sticks > 100) {
     printf("Error, number needs to be between 10 and 100 (inclusive)\n");
     exit(1);
   }
 
-  /* TODO: Main game loop:
-    While some sticks remain:
-      1. Human: Get number of sticks by calling getUserChoice
-      2. Computer: get number of sticks by calling getComputerChoice
-      3. Output the computer's choice, e.g., "Computer selects 3."
-      3. Update number_sticks
-      4. Print the current number of sticks, e.g., "There are 2 sticks on the board."
-    After all the sticks are gone, output the result of the game, 
-    e.g., "Computer wins." or "Computer loses."
-   */
-  int human_last_choice = 1;
+  // treat this as a boolean value to see who took the last stick
+  int human_last_choice = 1;  
+  
+  // do following loop until some player picks up last of sticks
   while(number_sticks > 0) {
     printf("\nThere are %d sticks on the board.\n", number_sticks);
     int human_choice = getUserChoice();
+
     number_sticks = number_sticks - human_choice;
     human_last_choice = 1;
+
+    // if human picked up last of sticks, break while loop
     if(number_sticks == 0) {
       break;
     }
 
+    // same steps for computer choice, update boolean value to 0 (imitate false)
     printf("\nThere are %d sticks on the board.\n", number_sticks);
     int computer_choice = getComputerChoice(number_sticks);
     printf("Computer selects %d\n", computer_choice);
+
     number_sticks = number_sticks - computer_choice;
     human_last_choice = 0;
   }
 
+  // depending on boolean value, human either wins or loses!
   if (human_last_choice) {
     printf("You lose!\n");
   }
