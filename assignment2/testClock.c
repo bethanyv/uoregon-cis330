fe1f34997704adf45ded3f267a5baa759e254602
Bethany <bethanyv21@gmail.com>
2019-01-23 13:39:14 -0800

    updating

--- assignment2/testClock.c
+++ assignment2/testClock.c
@@ -3,12 +3,14 @@
 #include <time.h>      
 #include <string.h>
 #include "clock.h"
-#include "clock.c"
 
 int main(int argc, char** argv) 
 {
   ClockType myclock;
   initClock(&myclock);
+  
+  time_t now = time(NULL);
+  printClock(now, &myclock);
 
   cleanClock(&myclock);
   return 0;
