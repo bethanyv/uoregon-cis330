f655bf1d0474dd3e6e997abe28cd665c651279e5
Bethany <bethanyv21@gmail.com>
2019-01-25 14:15:03 -0800

    Adding lab 3 files

---
+++ lab3/power.c
@@ -0,0 +1,6 @@
+#include <math.h>
+
+double power(double a, double b) {
+	double x = pow(a, b);
+	return x;
+}
---
+++ lab3/power.h
@@ -0,0 +1,6 @@
+#ifndef POWER_H_
+#define POWER_H_
+
+double power(double a, double b);
+
+#endif /* CLOCK_H_ */
