f77add4d013d16622fdababe96e13badfdcdc107
Bethany <bethanyv21@gmail.com>
2019-01-29 17:38:40 -0800

    update

---
+++ assignment3/reversi.c
@@ -0,0 +1,29 @@
+#include "reversi.h"
+
+void initBoard(BoardType *board, int size) {
+
+}
+
+void runGame(BoardType *board) {
+
+}
+
+void printBoard(const BoardType *board, size) {
+
+}
+
+void setPieces(BoardType *board) {
+
+}
+
+int main(int argc, char const *argv[]) {
+	/*
+	Ask players for input, 
+	make the board (init), 
+	initalize the first few pieces (setpieces)
+	runGame (calls print board every turn)
+	*/
+
+	initBoard(BoardType *board);
+	return 0;
+}
