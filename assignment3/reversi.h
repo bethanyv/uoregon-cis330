3352072c90961077515e6323a78f30787b80cd60
Bethany <bethanyv21@gmail.com>
2019-01-29 19:18:09 -0800

    updating - changed parameters

--- assignment3/reversi.h
+++ assignment3/reversi.h
@@ -2,9 +2,9 @@
 #define REVERSI_H_
 
 typedef enum {
-		empty; // 0
-		black; // 1
-		white; // 2
+		empty, // 0
+		black, // 1
+		white, // 2
 	} piece;
 
 typedef struct {
@@ -27,7 +27,7 @@ void printBoard(const BoardType *board, const int size);
 
 // place the piece where the user specified by checking if it's valid and flipping
 // if a piece is placed, return 1. Else return 0 
-int placePiece(const BoardType *board, const enum piece, const int x, int const y);
+int placePiece(const BoardType *board, const piece color, const int x, const int y);
 
 // check if there is a valid vertical move - if it is, return the 
 // index of other piece closing off opponent's pieces. If not, return -1
@@ -43,7 +43,7 @@ int diagonalValid(const int start_index);
 
 // check if there are ANY valid moves available for a player (piece will be a color)
 // call all valid functions and if all of them return -1, no moves left
-int anyValidMoves(const BoardType *board, const enum piece);
+int anyValidMoves(const BoardType *board, const piece color);
 
 // flip the pieces in between the indexes
 void horizontalFlip(const int start_index, const int end_index);
