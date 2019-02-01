c931cc39fd9ca88da825ed1e0cd2106a1d474bc1
Bethany <bethanyv21@gmail.com>
2019-02-01 14:35:49 -0800

    adding cpp

---
+++ lab4/num.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+	int **num = nullptr;
+	int size = 5, i, j;
+
+	// Allocate memory for 2-D int array
+	num = new int * [size]
+	for (i = 0; i < size; i++)
+		num[i] = new int[size]
+
+	i = 0;
+        // Set values
+	for (j = 0; j < size; j++)
+		num[i][j] = i * size + j;
+
+	// Print values
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			printf("%d ", num[i][j]);
+		}
+		printf("\n");
+	}
+	i = 0;
+	// Deallocate memory
+	for (i = 0; i < size; i++)
+		delete [] num[i];
+	delete [] num;
+}
