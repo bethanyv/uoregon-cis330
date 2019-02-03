8e0c6200b1910f91e6df491639101d4a435ef486
Bethany <bethanyv21@gmail.com>
2019-02-03 15:33:47 -0800

    deleted main from debugging

--- assignment4/reversi.cpp
+++ assignment4/reversi.cpp
@@ -700,24 +700,3 @@ void BoardType::cleanBoard() {
 	// free the array that held all the pieces
 	delete [] pieces;
 }
-
-// int main(int argc, char const *argv[]) {
-// 	// make the board from specified board size, set initial pieces, and run the game. Clean after 
-// 	int size;
-
-// 	std::cout << "Please enter the size of the board (4-16): ";
-// 	std::cin >> size;
-
-// 	while(size < 4 || size > 16) {
-// 		std::cout << "Invalid board size, please input again (4-16)\n";
-// 		std::cout << "Please enter the size of the board: ";
-// 		std::cin >> size;
-// 	}
-
-// 	BoardType board(size);
-// 	board.setPieces();
-// 	board.runGame();
-
-// 	board.cleanBoard();
-// 	return 0;
-// }
