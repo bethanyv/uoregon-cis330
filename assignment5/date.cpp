f85699eb0527dd9a0066ec36f8a3b58a15a8521a
Bethany <bethanyv21@gmail.com>
2019-02-19 12:14:38 -0800

    added more code

--- assignment5/date.cpp
+++ assignment5/date.cpp
@@ -11,9 +11,13 @@ Date::Date() : Cipher(), date("04/21/98") {
 std::string
 Date::encrypt( std::string &inputText ) {
 	std::string text = inputText;
-    std::string::size_type len_date = text.length();
-    std::string number_str;
-    
+
+    std::string::size_type len_date = Date->date.length();
+    std::string::size_type len = text.length();
+
+    std::string number_str; // ex. 042198
+    std::string completed_numbers; // ex. 0 41 9842 198
+
     for(int i = 0; i != len_date; ++i) {
         if(text[i] != '/') {
             number_str += text[i];
@@ -21,8 +25,23 @@ Date::encrypt( std::string &inputText ) {
     }
 	
     for (int i = 0; i != len; ++i) {
-        
+        if(text[i] != ' ') {
+            completed_numbers += number_str[i % 6];
+        }
+        else if (text[i] == ' ') {
+            completed_numbers += ' ';
+        }
     }
+    printf("%s\n", completed_numbers);
+    // for (int i = 0; i != len; ++i) {
+    //     if(text[i] != ' ') {
+    //         if (text[i] >= 'a' && text[i] <= 'z') {
+    //         }
+    //         else if (text[i] >= 'A' && text[i] <= 'Z') {
+
+    //         }
+    //     }
+    // }
 	return text;
 }
 
