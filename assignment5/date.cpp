93fa35a3ebc45366a4434e31bcb08684ab5527aa
Bethany <bethanyv21@gmail.com>
2019-02-19 11:46:45 -0800

    added some code to date.cpp

--- assignment5/date.cpp
+++ assignment5/date.cpp
@@ -4,15 +4,25 @@
 #include "date.hpp"
 
 // Single-argument constructor, initialized key to 2
-Date::Date() : Cipher() {
+Date::Date() : Cipher(), date("04/21/98") {
 }
 
 
 std::string
 Date::encrypt( std::string &inputText ) {
-
 	std::string text = inputText;
+    std::string::size_type len_date = text.length();
+    std::string number_str;
+    
+    for(int i = 0; i != len_date; ++i) {
+        if(text[i] != '/') {
+            number_str += text[i];
+        }
+    }
 	
+    for (int i = 0; i != len; ++i) {
+        
+    }
 	return text;
 }
 
