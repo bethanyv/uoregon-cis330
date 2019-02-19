8c47bed7ba5e0ddfe215f2d76fff362256d65ae8
Bethany <bethanyv21@gmail.com>
2019-02-19 12:42:11 -0800

    made string of numbers from input

--- assignment5/date.cpp
+++ assignment5/date.cpp
@@ -2,9 +2,10 @@
 
 #include "cipher.hpp"
 #include "date.hpp"
+#include <iostream>
 
 // Single-argument constructor, initialized key to 2
-Date::Date() : Cipher(), date("04/21/98") {
+Date::Date() : Cipher(), date("042198") {
 }
 
 
@@ -12,27 +13,17 @@ std::string
 Date::encrypt( std::string &inputText ) {
 	std::string text = inputText;
 
-    std::string::size_type len_date = Date->date.length();
     std::string::size_type len = text.length();
 
-    std::string number_str; // ex. 042198
+    std::string number_str = this->date; // ex. 042198
     std::string completed_numbers; // ex. 0 41 9842 198
-
-    for(int i = 0; i != len_date; ++i) {
-        if(text[i] != '/') {
-            number_str += text[i];
-        }
-    }
 	
     for (int i = 0; i != len; ++i) {
-        if(text[i] != ' ') {
+        if(text[i] >= 'a' && text[i] <= 'z' || text[i] >= 'A' && text[i] <= 'Z') {
             completed_numbers += number_str[i % 6];
         }
-        else if (text[i] == ' ') {
-            completed_numbers += ' ';
-        }
     }
-    printf("%s\n", completed_numbers);
+    
     // for (int i = 0; i != len; ++i) {
     //     if(text[i] != ' ') {
     //         if (text[i] >= 'a' && text[i] <= 'z') {
@@ -42,6 +33,7 @@ Date::encrypt( std::string &inputText ) {
     //         }
     //     }
     // }
+    
 	return text;
 }
 
