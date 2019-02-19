113296796fc254466cb55ee2506a6d1d1208c439
Bethany <bethanyv21@gmail.com>
2019-02-19 12:48:20 -0800

    added more code, almost done

--- assignment5/date.cpp
+++ assignment5/date.cpp
@@ -24,16 +24,23 @@ Date::encrypt( std::string &inputText ) {
         }
     }
     
-    // for (int i = 0; i != len; ++i) {
-    //     if(text[i] != ' ') {
-    //         if (text[i] >= 'a' && text[i] <= 'z') {
-    //         }
-    //         else if (text[i] >= 'A' && text[i] <= 'Z') {
+    int counter = 0; // for indexing into number_str
+    for (int i = 0; i != len; ++i) {
+        if(text[i] != ' ') {
+            counter += 1;
+            // lowercase
+            if (text[i] >= 'a' && text[i] <= 'z') {
+                // change index from number_str into int and index into LOWER and change text[i] to this letter
+                
+            }
+            // uppercase
+            else if (text[i] >= 'A' && text[i] <= 'Z') {
+                // change index from number_str into int and index into HIGHER and change text[i] to this letter
 
-    //         }
-    //     }
-    // }
-    
+            }
+        }
+    }
+
 	return text;
 }
 
