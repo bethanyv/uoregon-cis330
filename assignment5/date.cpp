3b69416cbcab805f3cc10aa46bc92e3d8fe356dc
Bethany <bethanyv21@gmail.com>
2019-02-20 01:42:20 -0800

    adding notes

--- assignment5/date.cpp
+++ assignment5/date.cpp
@@ -29,19 +29,30 @@ Date::encrypt( std::string &inputText ) {
     // we don't have a count)
     int counter = 0; 
     for (int i = 0; i != len; ++i) {
-        int add_index; // for the numerical value of the index in the number_str ("4" in "421")
-        if(text[i] >= 'a' && text[i] <= 'z' || text[i] >= 'A' && text[i] <= 'Z') { // if 
+
+        // add_index for the numerical value of the index in the number_str ("4" in "421")
+        int add_index; 
+
+        // if the character in the string is a letter, then encrypt it (otherwise don't do anything)
+        if(text[i] >= 'a' && text[i] <= 'z' || text[i] >= 'A' && text[i] <= 'Z') { 
+
+            // subtract 48 because completed_numbers[counter] gets casted from a string to
+            // an int, so we have to subtract 48 since ASCII codes of string numbers start at 48
             add_index = completed_numbers[counter] - 48;
+            // add to counter to be able to index in next index in next iteration
             counter += 1;
+
             // lowercase
             if (text[i] >= 'a' && text[i] <= 'z') {
-                // change index from number_str into int and index into LOWER and change text[i] to this letter
+                // index into LOWER from finding current character and change text[i] to this
+                // letter's index PLUS the add_index from the date number
                 std::size_t found_index = this->LOWER.find(text[i]);
                 text[i] = LOWER[(found_index + add_index) % 26];
             }
+            
             // uppercase
             else if (text[i] >= 'A' && text[i] <= 'Z') {
-                // change index from number_str into int and index into HIGHER and change text[i] to this letter
+                // same as LOWER except indexing into UPPER string instead
                 std::size_t found_index = this->UPPER.find(text[i]);
                 text[i] = UPPER[(found_index + add_index) % 26];
             }
